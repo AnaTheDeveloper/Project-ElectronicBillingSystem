bf52e9ca2d5b8dd2cddf5eb19440c7dd0270a8dc
MinniesTea <edwards.ana81@gmail.com>
2021-05-22 11:57:19 +0100

    With price included

--- src/main/java/AccountInfomation.java
+++ src/main/java/AccountInfomation.java
@@ -5,22 +5,26 @@ public class AccountInfomation {
   private String name = "";
   private double energyUsed = 0;
   private char energySymbol = 'W';
+  private double energyRate = 1.25;
+
+  //Getters and setters
 
   //Constructor Method
 
-  public AccountInfomation(String name, double energyUsed, char energySymbol)
+  public AccountInfomation(String name, double energyUsed, char energySymbol, double energyRate)
   {
     this.name = name;
     this.energyUsed = energyUsed;
     this.energySymbol = energySymbol;
+    this.energyRate = energyRate;
   }
 
   //Method Number 1
 
   public void energyCalculation(){
 
-    double energyForumula = energyUsed * 1.25;
-    System.out.println(name + ", you have used " + energyUsed + energySymbol + " of energy this month.");
+    double energyForumula = energyUsed * energyRate;
+    System.out.println(name + ", you have used " + energyUsed + energySymbol + " of energy this month. You now owe: £ " + energyForumula);
   }
 
 
--- src/main/java/ElectronicBillingSystemMain.java
+++ src/main/java/ElectronicBillingSystemMain.java
@@ -3,13 +3,17 @@ public class ElectronicBillingSystemMain {
 
   public static void main(String[] args) {
 
-    AccountInfomation anasConsumption = new AccountInfomation("Ana", 1250, 'W');
-    AccountInfomation lukesConsumption = new AccountInfomation("Luke", 1000, 'W');
-    AccountInfomation bobsConsumption = new AccountInfomation("Bob", 100.50, 'W');
+    AccountInfomation anasConsumption = new AccountInfomation("Ana", 1250, 'W', 1.25);
+    AccountInfomation lukesConsumption = new AccountInfomation("Luke", 1000, 'W', 1.25);
+    AccountInfomation bobsConsumption = new AccountInfomation("Bob", 100.50, 'W', 1.25);
 
     anasConsumption.energyCalculation();
     lukesConsumption.energyCalculation();
     bobsConsumption.energyCalculation();
+
+
+
+
 
   }
 
